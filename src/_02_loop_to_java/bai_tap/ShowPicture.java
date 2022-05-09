780a6940e9c9d98127864ca45c0f20be755e3abe
Thành Tri <apple@ThanhTri.local>
2022-05-09 17:00:08 +0700

    sua bai

--- src/_02_loop_to_java/bai_tap/ShowPicture.java
+++ src/_02_loop_to_java/bai_tap/ShowPicture.java
@@ -22,9 +22,38 @@ public class ShowPicture {
                     }
                     break;
                 case 2:
-                    for (int i = 1; i <=5 ; i++) {
-                        for (int j = 1; j <=i ; j++) {
-                            System.out.print("* ");
+                    for (int i = 0; i < 6; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print(" * ");
+                        }
+                        System.out.println("");
+                    }
+
+                    System.out.println("");
+                    for (int i = 0; i < 6; i++) {
+                        for (int j = 5; j > i; j--) {
+                            System.out.print(" * ");
+                        }
+                        System.out.println("");
+                    }
+
+                    for (int i = 1; i < 6; i++) {
+                        for (int j = 0; j < 5 - i; j++) {
+                            System.out.print("\t");
+                        }
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*   ");
+                        }
+                        System.out.println("");
+                    }
+
+                    System.out.println("");
+                    for (int i = 0; i < 6; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("\t");
+                        }
+                        for (int j = 5; j > i; j--) {
+                            System.out.print("*   ");
                         }
                         System.out.println("");
                     }break;
---
+++ src/_04_class_to_java/bai_tap/StopWatch.java
@@ -0,0 +1,5 @@
+package _04_class_to_java.bai_tap;
+
+public class StopWatch {
+
+}
